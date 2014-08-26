76654cd085478d18ace15fd0cde4faf18a38f951
wangjingbiao <wwwwjb123@126.com>
2014-08-26 18:39:32 +0800

    search，发现分类页面布局

---
+++ app/src/main/java/com/promote/ebingo/category/CategoryActivity.java
@@ -0,0 +1,18 @@
+package com.promote.ebingo.category;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.promote.ebingo.R;
+
+public class CategoryActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+
+
+}
---
+++ app/src/main/java/com/promote/ebingo/search/SearchCategoryPop.java
@@ -0,0 +1,13 @@
+package com.promote.ebingo.search;
+
+import android.widget.PopupWindow;
+
+/**
+ * Created by ACER on 2014/8/26.
+ */
+public class SearchCategoryPop extends PopupWindow{
+
+    public SearchCategoryPop(){
+
+    }
+}
