278b9f11419324a32a23ec4eb4afd2a4c7fc78f2
wangjingbiao <wwwwjb123@126.com>
2014-09-01 21:37:38 +0800

    修改 首页

---
+++ app/src/main/java/com/promote/ebingo/InformationActivity/InterpriseDynamicFragment.java
@@ -0,0 +1,67 @@
+package com.promote.ebingo.InformationActivity;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.promote.ebingo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link InterpriseDynamicFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ * 企業動態。
+ *
+ */
+public class InterpriseDynamicFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment InterpriseDynamic.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static InterpriseDynamicFragment newInstance(String param1) {
+        InterpriseDynamicFragment fragment = new InterpriseDynamicFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    public InterpriseDynamicFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_interprise_dynamic, container, false);
+    }
+
+
+}
