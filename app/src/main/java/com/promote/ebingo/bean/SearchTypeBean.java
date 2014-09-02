5bd9c8670a5cd6e906f4a3516376356f689ee0c9
wangjingbiao <wwwwjb123@126.com>
2014-09-02 21:38:54 +0800

    搜索页面

---
+++ app/src/main/java/com/promote/ebingo/bean/SearchTypeBean.java
@@ -0,0 +1,29 @@
+package com.promote.ebingo.bean;
+
+/**
+ * Created by ACER on 2014/9/2.
+ *
+ * 搜索內容基類。
+ */
+public class SearchTypeBean {
+
+    protected int id;
+
+    protected String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/promote/ebingo/impl/SearchDao.java
@@ -0,0 +1,71 @@
+package com.promote.ebingo.impl;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.promote.ebingo.bean.SearchHistoryBean;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ACER on 2014/9/2.
+ */
+public class SearchDao {
+
+    private static DbOpenHelper mHelper ;
+    private Context mContext;
+
+    public SearchDao(Context context){
+
+        this.mContext = context;
+        mHelper = new DbOpenHelper(context);
+    }
+
+    /**
+     * 保存历史记录。
+     * @param history
+     */
+    public synchronized void addHistory(String history){
+
+
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("history", history);
+        db.insert(DbOpenHelper.SEARCH_HISTORY, null, values);
+        db.close();
+    }
+
+    /**
+     * 获取历史记录。
+     * @return ArrayList<SearchHistoryBean>
+     */
+    public ArrayList<SearchHistoryBean> getHistorys(){
+
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        Cursor cursor = db.query(true, DbOpenHelper.SEARCH_HISTORY, new String[]{"history"}, null, null, null, null, "id desc", String.valueOf(7));
+        ArrayList<SearchHistoryBean> histories = new ArrayList<SearchHistoryBean>();
+
+        while (cursor.moveToNext()){
+            SearchHistoryBean historyBean = new SearchHistoryBean();
+            historyBean.setHistory(cursor.getString(cursor.getColumnIndex("history")));
+            histories.add(historyBean);
+        }
+
+        cursor.close();
+        db.close();
+        return histories;
+    }
+
+    /**
+     * 清空历史记录。
+     */
+    public synchronized void clearHistory(){
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        String sqlStr = "delete from " +DbOpenHelper.SEARCH_HISTORY;
+        db.execSQL(sqlStr);
+        db.close();
+    }
+
+}
