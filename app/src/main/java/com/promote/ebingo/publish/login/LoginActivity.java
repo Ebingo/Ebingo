a96e2b9db7c74049764a4d2e480736537c0040a1
guochuanhu <771509565@qq.com>
2014-09-03 10:15:21 +0800

    zhuchao93-1

---
+++ app/src/main/java/com/promote/ebingo/publish/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.promote.ebingo.publish.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.promote.ebingo.R;
+
+/**
+ * Created by acer on 2014/9/3.
+ */
+public class LoginActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/promote/ebingo/publish/login/LoginDialog.java
+++ app/src/main/java/com/promote/ebingo/publish/login/LoginDialog.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -33,6 +34,7 @@ public class LoginDialog extends Dialog implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login_dialog);
         initWidgets();
     }
