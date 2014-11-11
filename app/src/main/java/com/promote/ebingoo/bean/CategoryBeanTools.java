ee0cdf6aa883a1311477e579299e1c8943a695d0
wangjingbiao <wwwwjb123@126.com>
2014-11-11 17:42:12 +0800

    添加发现也的子分类功能

--- app/src/main/java/com/promote/ebingoo/bean/CategoryBeanTools.java
+++ app/src/main/java/com/promote/ebingoo/bean/CategoryBeanTools.java
@@ -21,11 +21,58 @@ public class CategoryBeanTools {
     }
 
 
-    public static ArrayList<CategoryBeen> getCategories(String json){
+    public static ArrayList<CategoryBeen> getCategories(String json) {
         Gson gson = new Gson();
-        Type type =new  TypeToken<CategoryBeanTools>(){}.getType();
+        Type type = new TypeToken<CategoryBeanTools>() {
+        }.getType();
 
-        return ((CategoryBeanTools)gson.fromJson(json, type)).getResponse();
+
+        ArrayList<CategoryBeen> categoryBeens = ((CategoryBeanTools) gson.fromJson(json, type)).getResponse();
+
+
+        return divideSubCategory(categoryBeens);
     }
+
+    /**
+     * 将子分类添加到父分类中。
+     *
+     * @param categoryBeens
+     * @return
+     */
+    private static ArrayList<CategoryBeen> divideSubCategory(ArrayList<CategoryBeen> categoryBeens) {
+
+        ArrayList<CategoryBeen> categoryBeens_copy = new ArrayList<CategoryBeen>(categoryBeens);
+
+        int parentId;
+        for (CategoryBeen category : categoryBeens) {
+            parentId = category.getParent_id();
+            if (parentId != 0) {    //子分类。
+                categoryBeens_copy.remove(category);
+                SubCategoryBean subCategoryBean = new SubCategoryBean();
+                subCategoryBean.setName(category.getName());
+                subCategoryBean.setParent_id(category.getParent_id());
+                subCategoryBean.setId(category.getId());
+                getCategoryByParentId(parentId, categoryBeens_copy).getSubCategorys().add(subCategoryBean);
+            }
+
+        }
+
+        return categoryBeens_copy;
+
+    }
+
+    private static CategoryBeen getCategoryByParentId(int id, ArrayList<CategoryBeen> categoryBeens) {
+
+        for (CategoryBeen categoryBeen : categoryBeens) {
+
+            if (categoryBeen.getId() == id) {
+                return categoryBeen;
+            }
+
+        }
+
+        return null;
+    }
+
 
 }
